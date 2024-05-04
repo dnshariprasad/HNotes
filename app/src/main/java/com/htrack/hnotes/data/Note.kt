fbdc5d1a375c8a4ce1f79e0ac728b8da1bdfa401
Hari <dnshariprasad@gmail.com>
2024-05-04 14:13:52 +0530

    Note objects added

--- app/src/main/java/com/htrack/hnotes/data/Note.kt
+++ app/src/main/java/com/htrack/hnotes/data/Note.kt
@@ -4,6 +4,8 @@ data class Note(
     val id: String? = "",
     val title: String? = "",
     val info: String? = "",
+    val type: String? = "",
+    val time: String? = "",
     val tags: List<Tag>? = emptyList()
 )
 
