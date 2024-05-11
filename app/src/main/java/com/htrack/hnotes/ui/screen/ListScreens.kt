4d8fc32e8f9ea3ecf4d0e8f4c910fec03e5cec25
Hari <dnshariprasad@gmail.com>
2024-05-11 12:59:32 +0530

    UI fixes

--- app/src/main/java/com/htrack/hnotes/ui/screen/ListScreens.kt
+++ app/src/main/java/com/htrack/hnotes/ui/screen/ListScreens.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
@@ -130,9 +131,10 @@ fun NoteItem(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 1.dp
         ),
+        shape = RoundedCornerShape(4.dp),
         border = BorderStroke(
             0.1.dp,
-            Color.Gray
+            Color.Gray,
         ),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.background
